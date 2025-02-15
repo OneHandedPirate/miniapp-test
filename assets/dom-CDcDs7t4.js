e1d58bc08832f9803877b639e4a09170a50db91f
onehandedpirate <plagueismkii@gmail.com>
2025-02-15 10:40:07 +1000

    Updates

--- assets/dom-CDcDs7t4.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./index-B1BSuSKs.js";function c(t,r){const n=t.style;for(const o in r)n[o]=r[o]}function u(t){if(t==null)return;if(typeof t=="string")try{return document.querySelector(t)||void 0}catch{return}const r=e(t);if(r)return r.$el||r}export{c,u as g};
